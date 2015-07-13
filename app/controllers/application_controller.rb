ee7f1a46cd0f6c700f46da8afec1ee1eee0e350f
Nidhi Makhijani <nidhimj22@gmail.com>
2015-07-13 15:44:45 +0530

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,32 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   rescue_from Exception, :with => :record_not_found
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from CarrierWave::IntegrityError, with: :invalid_file
 
   def record_not_found
-    flash[:error] = "Oops! We cannot find what you are looking for!"
+    flash[:error] = "Record Not Found"
     redirect_to new_user_session_path
     return
   end
+
 
   def invalid_file
     redirect_to new_user_attachment_path, alert: "File format not supported"
   end
 
+
   def route_user
     if !current_user.nil?
       if !params.empty? && params.key?(:user_id) && current_user.id.to_s != params[:user_id]
-        redirect_to user_attachments_path(current_user.id), alert: "Unauthourized!!"
+        redirect_to user_attachments_path(current_user.id), alert: "Unauthourized user ... Login or Signup"
       end
     end
   end
 
+
   protect_from_forgery with: :exception
+
   def after_sign_in_path_for(user)
     user_attachments_path(user)
   end
@@ -35,9 +38,10 @@ class ApplicationController < ActionController::Base
   def validate_file_size(file)
     one_mb = 1048567
     if file.size > 2*one_mb
-      redirect_to user_attachments_path, alert: "Too large. Should be < 2MB"
+      redirect_to user_attachments_path, alert: "File Size Should be below 2MB"
       return false
     end
     true
   end
+
 end
